8ed50b46a6cfd64b2392ed70d2ec5187b85f972c
benup <benup211@gmail.com>
2024-11-29 22:37:56 +0545

    add:bottomNavigation for admin page

--- client/Navigation.tsx
+++ client/Navigation.tsx
@@ -1,27 +1,68 @@
+import { DarkTheme,DefaultTheme } from '@react-navigation/native';
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import LoginPage from './screens/auth/LoginPage';
 import RegiserPage from './screens/auth/RegisterPage';
 import AdminLogin from './screens/auth/AdminLogin';
-import { DarkTheme,DefaultTheme } from '@react-navigation/native';
+import RegisteredUser from './screens/admin/RegisteredUser';
+import Restaurant from './screens/admin/Restaurant';
+import AdminSettings from './screens/admin/Settings';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-const Stack = createNativeStackNavigator();
-function AuthStack() {
+const AdminBottomTab = createBottomTabNavigator();
+function AdminGroup() {
     return (
-        <Stack.Navigator screenOptions={{
+        <AdminBottomTab.Navigator screenOptions={{
             headerShown: false
         }}>
-            <Stack.Screen name="Login" component={LoginPage} />
-            <Stack.Screen name="Register" component={RegiserPage} />
-            <Stack.Screen name="Admin" component={AdminLogin} />
-        </Stack.Navigator>
+            <AdminBottomTab.Screen name="Users" component={RegisteredUser} options={{
+                tabBarIcon: () => {
+                    return <AntDesign name="user" size={24} color="#8B5CFF" />
+                },
+                tabBarActiveTintColor: "#8B5CFF",
+            }} />
+            <AdminBottomTab.Screen name="Restaurant" component={Restaurant} options={{
+                tabBarIcon: () => {
+                    return <Ionicons name="restaurant-outline" size={24} color="#8B5CFF" />
+                },
+                tabBarActiveTintColor: "#8B5CFF",
+            }} />
+            <AdminBottomTab.Screen name="Settings" component={AdminSettings} options={{
+                tabBarIcon: () => {
+                    return <AntDesign name="setting" size={24} color="#8B5CFF" />
+                },
+                tabBarActiveTintColor: "#8B5CFF",
+            }} />
+        </AdminBottomTab.Navigator>
     )
 }
 
+const AuthStack = createNativeStackNavigator();
+function MainNavigator() {
+    return (
+        <AuthStack.Navigator screenOptions={{
+            headerShown: false
+        }}>
+            <AuthStack.Screen name="Login" component={LoginPage} />
+            <AuthStack.Screen name="Register" component={RegiserPage} />
+            <AuthStack.Screen name="Admin Login" component={AdminLogin} />
+            <AuthStack.Screen name="Admin" component={AdminGroup} />
+        </AuthStack.Navigator>
+    )
+}
+const CustomDarkTheme = {
+    ...DarkTheme,
+    colors: {
+      ...DarkTheme.colors,
+      background: '#1a202c',
+    },
+  };
 export default function Navigation() {
     return (
-        <NavigationContainer theme={DefaultTheme}>
-            <AuthStack />
+        <NavigationContainer theme={CustomDarkTheme}>
+            <MainNavigator />
         </NavigationContainer>
     )
 }
